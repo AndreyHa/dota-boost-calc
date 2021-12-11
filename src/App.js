9ba9096b32a617b2d3c4c4c6049e36d41e4d7dc9
Andrey Klygin <gfka63@gmail.com>
2021-12-12 01:04:19 +0700

    made UI

--- src/App.js
+++ src/App.js
@@ -16,30 +16,30 @@ const App = () => {
         ).then(result => setSum(result))
     }
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
+            <Input style={{ marginTop: '1em' }}
+                placeholder='begin mmr' value={beginMMR}
+                onChange={(e) => {
+                    setBeginMMR(e.target.value)
+                    calculateNewPrise(e.target.value, endMMR, isParty)
+                }}
+            ></Input>
+            <br />
+            <Input style={{ marginTop: '1em' }}
+                placeholder='end mmr' value={endMMR}
+                onChange={(e) => {
+                    setEndMMR(e.target.value)
+                    calculateNewPrise(beginMMR, e.target.value, isParty)
+                }}
+            ></Input>
             <div>
-                <Input placeholder='begin mmr' value={beginMMR}
-                    onChange={(e) => {
-                        setBeginMMR(e.target.value)
-                        calculateNewPrise(e.target.value, endMMR, isParty)
-                    }}
-                ></Input>
-            </div>
-            <div>
-                <Input placeholder='end mmr' value={endMMR}
-                    onChange={(e) => {
-                        setEndMMR(e.target.value)
-                        calculateNewPrise(beginMMR, e.target.value, isParty)
-                    }}
-                ></Input>
-            </div>
-            <div>
-                <h3>патибуст</h3>
-                <Checkbox value={isParty}
+                <Checkbox value={isParty} label='патибуст'
                     onChange={(e, w) => {
                         setIsParty(w.checked)
                         calculateNewPrise(beginMMR, endMMR, w.checked)
-                    }}></Checkbox>
+                    }}
+                    style={{ marginTop: '1em' }}
+                />
             </div>
             <h2>Цена для клиента: {sum}</h2>
             <h2>Цена для бустера: {sum * 0.9}</h2>
